0a76c5ea8ae3bb9cd2345c5dc50693b028c1af34
Ivan Novikov <jonny.novikov@gmail.com>
2023-01-21 22:44:20 +0300

    assets as a package

---
+++ packages/assets/vite.config.js
@@ -0,0 +1,16 @@
+import { useDefaultViteConfig } from "../packageConfig";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+import { normalizePath } from "vite";
+import path from "node:path";
+
+export default useDefaultViteConfig("assets", [
+  viteStaticCopy({
+    targets: [
+      {
+        src: normalizePath(path.resolve(__dirname, "./images/*")),
+        dest: "images",
+      },
+    ],
+  }),
+]);
